52769ae3016e2456af97da38207f6c255883f7b4
marvelousajuzie <Chizurummarvelous14@gmail.com>
2024-09-05 08:40:50 +0100

    added

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,44 @@ void main() => runApp(
     enabled: !kReleaseMode,
     builder: (context) =>  MaterialApp(
       useInheritedMediaQuery: true,
-      home: Scaffold()
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First Flutter Application'),
+          centerTitle: true,
+          backgroundColor: Colors.lightBlue[500],
+        ),
+        body: const Center(
+          child: Text(
+            "Hello Programmers",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.black,
+              fontFamily: 'Indieflower',
+            ),
+            ),
+        ), 
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {},
+        //   child: Text('Click'),
+        //   backgroundColor: Colors.lightBlue[500],
+        // ),
+        bottomNavigationBar:  BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+               icon: Icon(Icons.home) 
+              ),
+
+              BottomNavigationBarItem(
+                label: 'Settings',
+                icon: (Icon(Icons.settings)))
+                
+            ],
+            backgroundColor: Colors.lightBlue[500],
+          ),
+      )
       ), // Wrap your app
   ),
 );
