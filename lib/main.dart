231b5bc8cce241e85139dd840a328057d567f7c4
marvelousajuzie <Chizurummarvelous14@gmail.com>
2024-09-07 00:11:36 +0100

    add

--- lib/main.dart
+++ lib/main.dart
@@ -23,10 +23,71 @@ class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[500],
       appBar: AppBar(
         title: Text('Ninja ID Card'),
         centerTitle: true,
         backgroundColor: Colors.pinkAccent,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+            Text(
+              'NAME:',
+              style: TextStyle(
+                color: Colors.pinkAccent,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'Marvelous Ajuzie',
+              style: TextStyle(
+                color: Colors.black,
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+             SizedBox(height: 20.0,),
+             Text(
+              'CURRENT NINJA LEVEL:',
+              style: TextStyle(
+                color: Colors.pinkAccent,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              '80.5',
+              style: TextStyle(
+                color: Colors.black,
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+             SizedBox(height: 10.0,),
+            Row(
+              children: <Widget> [
+                Icon(
+                  Icons.email,
+                  color: Colors.black,
+                ),
+                Text(
+                  'Marvelous12@gmail.com',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontsize : 18.0,
+                  ),
+                ),
+              ],
+              )
+          ],
+        ),
       ),
     );
   }
